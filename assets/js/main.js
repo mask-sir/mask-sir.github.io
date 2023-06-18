43a977de9ff2b136f18d3937da9a909e310fc472
mask ser <appushijil72@gmail.com>
2023-06-18 20:31:32 +0530

    project && contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,34 @@ window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
 
-
+const contactForm = document.getElementById('contact-form')
+const contactMessage = document.getElementById('contact-message'),
+const sendEmail = (e) =>{
+	e.preventDefault()
+	
+	// serviceID - templateID - #form - publicKey
+	emailjs.sendForm('service_5a6zu0c', 'template_oj2bz0p','#contact--form','5O8iRNVFb6tp51ckr')
+	.then(() =>{ 
+		// Show sent message
+		contactMessage.textContent = 'Message sent successfully✅'
+		
+		// Remove message after five seconds
+		setTimeout(()=>{
+			contactMessage.textContent = ''
+			}, 5000)
+			
+			// Clear input fields
+			
+			contactForm.reset()
+		
+		}, () => {
+			// Show error message
+			contactMessage.textContent = 'Message not sent (service error)❌'
+			});
+	}
+	contactForm.addEventListener('submit', sendEmail)
+	
+	
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
