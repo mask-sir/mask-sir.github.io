01826656e62eddbc670c4c5fa895f17f320f142f
mask ser <appushijil72@gmail.com>
2023-06-18 10:02:19 +0530

    clear msg

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,6 +51,16 @@ const sendEmail = (e) =>{
 	.then(() =>{ 
 		// Show sent message
 		contactMessage.textContent = 'Message sent successfully✅'
+		
+		// Remove message after five seconds
+		setTimeout(()=>{
+			contactMessage.textContent = ''
+			}, 5000)
+			
+			// Clear input fields
+			
+			contactForm.reset()
+		
 		}, () => {
 			// Show error message
 			contactMessage.textContent = 'Message not sent (service error)❌'
